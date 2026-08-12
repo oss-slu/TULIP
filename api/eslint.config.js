4a5c86440229fcf92b585f23179b912856bb4ee1
Bella Ott <bella.c.ott.04@gmail.com>
2026-08-11 22:27:03 -0500

    add linting for api

---
+++ api/eslint.config.js
@@ -0,0 +1,41 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    ignores: ["**/static/tabler.js"],
+  },
+  {
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "error",
+      "max-len": [
+        "error",
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      // "max-lines-per-function": [
+      //   "warn",
+      //   {
+      //     max: 100,
+      //     skipBlankLines: true,
+      //     skipComments: true,
+      //   },
+      // ],
+    },
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js"], // Match test files
+    rules: {
+      "max-lines-per-function": "off", // Disable the rule for test files
+    },
+  },
+];
--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,21 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+    ignores: ["**/static/tabler.js"],
+  },
+  {
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "error",
     },
   },
-])
+];
